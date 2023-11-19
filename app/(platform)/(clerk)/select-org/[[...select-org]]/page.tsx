77b18968a36fd6c3b422d3e58f7f8906ff7399ef
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2023-11-19 20:14:18 +0200

    first commit

---
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -0,0 +1,6 @@
+import { OrganizationList } from "@clerk/nextjs";
+
+
+export default function CreateOrganizationList (){
+    return <OrganizationList hidePersonal afterSelectOrganizationUrl="/organization/:id" afterCreateOrganizationUrl="/organization/:id"/>
+}
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db"
+
+const OrganizationIdPage = ()=>{
+    async function create(formData:FormData) {
+        "use server"
+
+        const title = formData.get("title") as string
+
+        db.board.create({
+            data:{
+                title
+            }
+        })
+    }
+
+    return (
+        <div>
+            <form action={create}>
+                <input id="title" name="title" required placeholder="Enter a board title" className="border-black border p-1"/>
+            </form>
+        </div>
+    )
+}
+
+export default OrganizationIdPage
